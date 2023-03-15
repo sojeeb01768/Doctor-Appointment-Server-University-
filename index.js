be35402fe1b4318c33938d0bdb8ead7d96737e1a
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-03-16 03:41:32 +0600

    setup db and backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -9,3 +10,31 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DBUser}:${process.env.DBPassword}@cluster0.kvqywrf.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const consultationCollection = client.db('university-project').collection('consultation');
+
+        app.get('/consultation', async (req, res) => {
+            const query = {};
+            const consultation = await consultationCollection.find(query).toArray();
+            res.send(consultation);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal is running')
+})
+
+app.listen(port, () => console.log(`doctors portal running on ${port}`))
