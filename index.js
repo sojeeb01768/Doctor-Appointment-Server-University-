9313fde9714e2a6fba1bebb27489a0b540544a81
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-04-17 14:16:32 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { query } = require('express');
 const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
@@ -24,6 +25,26 @@ async function run() {
         const doctorsCollection = client.db('university-project').collection('doctors');
         const bookingsCollection = client.db('university-project').collection('bookings');
         const usersCollection = client.db('university-project').collection('users');
+
+
+        //NOTE:  make sure you use varifyAdmin after verifyJWT
+        const verifyJWT = async (req, res, next) => {
+            // console.log("inside verifyJWT", req.headers.authorization);
+            const authHeader = req.headers.authorization;
+
+            // const decodedEmail = req.decoded.email;
+            // const query = { email: decodedEmail };
+            // const user = await usersCollection.findOne(query);
+
+            if (!authHeader) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+
+            const token = authHeader.split('')[1];
+
+            // next()
+        }
+
 
         // consultation data
         app.get('/consultation', async (req, res) => {
@@ -66,17 +87,36 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
+        // jwt token access for user
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+                return res.send({ accessToken: token });
+            }
+            console.log(user);
+            res.status(403).send({ accessToken: '' })
+
+        })
 
         // save user data to database
         app.post('/users', async (req, res) => {
-            const user=req.body;
+            const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+        // get booking data to dashboard
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            // console.log('token', req.headers.authorization);
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
     }
-
-
     finally {
 
     }
