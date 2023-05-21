ab8406b8b919cf1fbf01d68e9d3408446e677f8f
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-05-21 14:25:17 +0600

    appointment backend

--- index.js
+++ index.js
@@ -40,10 +40,18 @@ async function run() {
                 return res.status(401).send({ message: 'unauthorized access' })
             }
 
-            const token = authHeader.split('')[1];
+            const token = authHeader.split(' ')[1];
 
-            // next()
+            jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+                if (err) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
         }
+
+
 
 
         // consultation data
@@ -70,6 +78,37 @@ async function run() {
             res.send(doctors)
 
         });
+        //use aggregate to query multiple collection and then merge data
+        //get all doctors
+        app.get('/doctors', async (req, res) => {
+            const date = req.query.date;
+            const search = req.query.search;
+            console.log(search);
+            let query = {};
+            if (search.length) {
+                query = {
+                    $text: {
+                        $search: search
+                    }
+                }
+            }
+            const doctors = await doctorsCollection.find(query).toArray();
+
+            //get the booking of the provided date
+            const bookingQuery = { appointmentDate: date }
+            const bookedSlot = await bookingsCollection.find(bookingQuery).toArray();
+            // query for removing booked slot and show available slot
+            doctors.forEach(doctor => {
+                const slotBooked = bookedSlot.filter(book => book.doctorName === doctor.name)
+                const bookedSlots = slotBooked.map(book => book.slot)
+                const remainingSlots = doctor.slots.filter(slot => !bookedSlots.includes(slot))
+                doctor.slots = remainingSlots;
+                // console.log(date, doctor.name, remainingSlots.length);
+            })
+            res.send(doctors);
+        })
+
+
         // get single doctor basis on id 
         app.get('/doctor-details/:id', async (req, res) => {
             const id = req.params.id;
@@ -83,7 +122,17 @@ async function run() {
         // post booking data to database
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
-            // console.log(booking);
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.patientEmail,
+                doctor: booking.doctorName
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            if (alreadyBooked.length) {
+                const message = `You  Already have an Booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
@@ -101,20 +150,68 @@ async function run() {
 
         })
 
-        // save user data to database
+        // get all users from data base
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+        // save users data to database
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
+        // secure admin route by varify admin
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.category === 'admin' });
+        })
+
+
+
+        // Make users admin 
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.category !== 'admin') {
+                return res.send(403).send({ message: 'forbidden access' })
+            }
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    category: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+
         // get booking data to dashboard
         app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            // console.log(email);
             // console.log('token', req.headers.authorization);
-            const query = { email: email };
+            const query = { patientEmail: email };
+            // console.log(query);
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
         })
+
+
 
     }
     finally {
